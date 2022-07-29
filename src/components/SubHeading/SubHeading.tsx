99f0ab522709eb8980c0fe0e890f65bdd4687357
Saladino Belisario <SaladinoBelisario@protonmail.ch>
2022-07-29 12:38:57 -0500

    feat(header): Add header
    Add header section to the main page

--- src/components/SubHeading/SubHeading.tsx
+++ src/components/SubHeading/SubHeading.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 
-const SubHeading = () => (
-  <div>
-    SubHeading
-  </div>
+import { images } from '../../constants';
+
+interface SubHeadingProperties {
+    title: string
+}
+
+const SubHeading = ({ title }: SubHeadingProperties) => (
+    <div style={{ marginBottom: '1rem' }}>
+        <p className="p__cormorant">{title}</p>
+        <img src={images.spoon} alt="spoon_image" className="spoon__img" />
+    </div>
 );
 
 export default SubHeading;
